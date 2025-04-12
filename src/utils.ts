888ba294da16ba044a5169e879bbd33bcfb6cbc9
Mark LauQ <mark.lauq@gmail.com>
2025-04-12 17:02:55 +0800

    feat(utils): getRecentDateString

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Get recent date string
+ *
+ * @param { year: number }
+ * @returns `YYYY-MM-DD`
+ */
+export function getRecentDateString({ year }: { year: number }): string {
+  const date = new Date();
+  const pastYear = date.getFullYear() - year;
+
+  return [
+    pastYear,
+    String(date.getMonth() + 1).padStart(2, "0"),
+    String(date.getDate()).padStart(2, "0"),
+  ].join("-");
+}
