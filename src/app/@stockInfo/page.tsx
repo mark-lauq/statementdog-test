247f52cf5ec85c62a4caa51ad44bf38880e0e5e8
Mark LauQ <mark.lauq@gmail.com>
2025-04-12 15:32:32 +0800

    style(StockInfo): use elevation variant

--- src/app/@stockInfo/page.tsx
+++ src/app/@stockInfo/page.tsx
@@ -2,7 +2,7 @@ import { Card, CardHeader } from "@mui/material";
 
 export default function StockInfo() {
   return (
-    <Card variant="outlined">
+    <Card>
       <CardHeader title="三商壽 (2867)" />
     </Card>
   );
