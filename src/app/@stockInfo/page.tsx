62a73445f51a7c5f2121f5518ae6c26c28c7c393
Mark LauQ <mark.lauq@gmail.com>
2025-04-13 20:54:34 +0800

    style(StockInfo): update title

--- src/app/@stockInfo/page.tsx
+++ src/app/@stockInfo/page.tsx
@@ -3,7 +3,10 @@ import { Card, CardHeader } from "@mui/material";
 export default function StockInfo() {
   return (
     <Card>
-      <CardHeader title="三商壽 (2867)" />
+      <CardHeader
+        sx={{ ".MuiCardHeader-title": { fontSize: "18px", fontWeight: 600 } }}
+        title="三商壽 (2867)"
+      />
     </Card>
   );
 }
