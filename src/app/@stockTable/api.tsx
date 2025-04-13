e6a0a582aa0ce8add54764af0c2ea0a0b6ffd183
Mark LauQ <mark.lauq@gmail.com>
2025-04-13 10:29:52 +0800

    refactor(StockTable): extract API

---
+++ src/app/@stockTable/api.tsx
@@ -0,0 +1,21 @@
+import { getRecentDateString } from "@/utils";
+
+export interface Data {
+  date: string;
+  revenue: number;
+}
+
+const { API_URL, API_TOKEN, DATASET, DATA_ID } = process.env;
+
+const searchParams = new URLSearchParams({
+  token: API_TOKEN!,
+  dataset: DATASET!,
+  data_id: DATA_ID!,
+  start_date: getRecentDateString({ year: 6 }),
+});
+
+export async function getData() {
+  const response = await fetch(`${API_URL}?${searchParams.toString()}`);
+  const { data } = (await response.json()) as { data: Data[] };
+  return data;
+}
