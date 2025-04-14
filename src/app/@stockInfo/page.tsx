7c995128c5d5cdd93f0f7a92a14b6ee15aa88e97
Mark LauQ <mark.lauq@gmail.com>
2025-04-14 18:40:36 +0800

    style(StockInfo): update typography

--- src/app/@stockInfo/page.tsx
+++ src/app/@stockInfo/page.tsx
@@ -2,6 +2,8 @@ import { Paper } from "@mui/material";
 
 export default function StockInfoPage() {
   return (
-    <Paper sx={{ fontSize: "18px", fontWeight: 600 }}>三商壽 (2867)</Paper>
+    <Paper sx={{ fontSize: "18px", fontWeight: 600, lineHeight: "18px" }}>
+      三商壽 (2867)
+    </Paper>
   );
 }
