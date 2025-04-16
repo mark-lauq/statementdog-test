6bf20602ce3fb07b676e518e729f94226338fc12
Mark LauQ <mark.lauq@gmail.com>
2025-04-16 19:58:43 +0800

    refactor(StockTable): remove layout

--- src/app/@stockTable/page.tsx
+++ src/app/@stockTable/page.tsx
@@ -1,8 +1,16 @@
+import { Paper, Tabs, Tab } from "@mui/material";
 import { getData } from "@/libs/data";
 import StockTableContainer from "./StockTableContainer";
 
 export default async function StockTablePage() {
   const data = await getData();
 
-  return <StockTableContainer data={data} />;
+  return (
+    <Paper>
+      <Tabs value={false}>
+        <Tab label="详细数据" />
+      </Tabs>
+      <StockTableContainer data={data} />
+    </Paper>
+  );
 }
