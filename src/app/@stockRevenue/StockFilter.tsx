ec022bd8b3105281b45aafe6f932c8b9478d63ac
Mark LauQ <mark.lauq@gmail.com>
2025-04-17 11:50:54 +0800

    style(StockFilter): code format

--- src/app/@stockRevenue/StockFilter.tsx
+++ src/app/@stockRevenue/StockFilter.tsx
@@ -1,25 +1,29 @@
 "use client";
 
 import { use } from "react";
-import { FormControl, Select, MenuItem } from "@mui/material";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  type SelectChangeEvent,
+} from "@mui/material";
 import { AppContext } from "../AppProvider";
+
+const OPTIONS = [1, 3, 5, 8];
 
 export default function StockFilter() {
   const { recentYear, setRecentYear } = use(AppContext)!;
 
+  const handleChange = ({ target }: SelectChangeEvent<number>) => {
+    setRecentYear(Number(target.value));
+  };
+
   return (
     <FormControl>
-      <Select
-        size="small"
-        value={recentYear}
-        onChange={({ target }) => {
-          setRecentYear(Number(target.value));
-        }}
-      >
-        <MenuItem value="1">近1年</MenuItem>
-        <MenuItem value="3">近3年</MenuItem>
-        <MenuItem value="5">近5年</MenuItem>
-        <MenuItem value="8">近8年</MenuItem>
+      <Select size="small" value={recentYear} onChange={handleChange}>
+        {OPTIONS.map((opt) => (
+          <MenuItem key={opt} value={opt}>{`近${opt}年`}</MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
